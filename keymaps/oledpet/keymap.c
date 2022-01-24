47a7f521bcc8ee115f6e0d2bc49e2445ab11d65b
robdog77 <88400137+robdog77@users.noreply.github.com>
2022-01-24 09:15:52 -0500

    Add files via upload

--- keymaps/oledpet/keymap.c
+++ keymaps/oledpet/keymap.c
@@ -47,18 +47,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-        KC_F1,   KC_F2, KC_F3,
-  KC_7, KC_8,     KC_9, KC_PSLS,
-  KC_4, KC_5,     KC_6, KC_PAST,
-  KC_1, KC_2,     KC_3, KC_PMNS,
-  KC_0, KC_DOT, KC_ENT, KC_PPLS
+        KC_MUTE,   KC_PSLS, KC_PAST,
+  KC_7, KC_8,     KC_9, KC_PMNS,
+  KC_4, KC_5,     KC_6, KC_PPLS,
+  KC_1, KC_2,     KC_3, KC_ENT,
+  MO(1), KC_0, KC_DOT, KC_ENT
   ),
 
   [_VIA1] = LAYOUT(
-           KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+           KC_MPLY, KC_TRNS, KC_DEL,
+  KC_HUI, KC_TRNS, KC_MOD, KC_VAI,
+  KC_HUD, KC_UP, KC_RMOD, KC_VAD,
+  KC_LEFT, KC_DOWN, KC_RIGHT, KC_TOG,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
